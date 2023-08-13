f06b60b89373c9c8d6f390189a25088cef79d9a6
Damien Nelson <dhnisaman@gmail.com>
2023-08-13 16:07:07 -0400

    wrap app in the provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import {BrowserRouter as Router} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
