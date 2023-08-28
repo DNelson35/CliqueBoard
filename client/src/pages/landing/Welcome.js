014b26e58d2cc4aa8aab6afe8b6db6ca7d059ca6
Damien Nelson <dhnisaman@gmail.com>
2023-08-28 15:41:28 -0400

    add image to welcome page

--- client/src/pages/landing/Welcome.js
+++ client/src/pages/landing/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import image from '../../assets/planning.png'
 
 function Welcome() {
 
@@ -8,11 +9,16 @@ function Welcome() {
 
   return (
    <div className='w-full'>
-      <div className='flex flex-col justify-center h-screen bg-pink-300'>
-        <div className='w-1/2 ml-40'>
+      <div className='flex justify-between h-screen bg-pink-300'>
+        <div className='flex flex-col justify-center w-1/2 ml-40'>
           <h1 className='text-white text-7xl font-bold'>Transform and Thrive</h1>
           <p className='pt-6 text-white text-xl font-bold w-[70%]'>Your All-in-One Solution for Family Organization - Seamlessly Plan Events, Manage Tasks, and Customize Your Family's Journey to Perfection.</p>
           <button className='h-12 w-40 mt-6 text-white text-2xl font-bold rounded-lg bg-gradient-to-t bg-blue-600 hover:from-blue-400 hover:to-blue-600  ' onClick={() => navigate('/signup')}>Sign up</button>
+          
+        </div>
+        <div className='flex flex-col justify-center w-1/2 '>
+        {/* TODO: replace image with better image */}
+          <img src={image} alt='planning' className='mt-20 h-3/4 w-full' />
         </div>
       </div>
       <div className="flex flex-col items-center justify-center h-screen bg-red-200">
