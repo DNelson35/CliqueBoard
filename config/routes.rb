908d7111dc8a13269c884e884b1f86da683f0cc0
Damien Nelson <dhnisaman@gmail.com>
2023-09-13 14:46:56 -0400

    update routes, update seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :tasks
-  # resources :groups
   # resources :group_widgets, only: [:create, :show, :index]
   # resources :widgets, only: [:create, :show, :index]
   resources :users, only: [:create, :show, :update ]
+  resources :groups, only: [:create, :show, :index]
 
-  patch 'users/:id/upload_image', to: 'users#upload_image'
+  # patch 'users/:id/upload_image', to: 'users#upload_image'
   post '/signup', to: 'users#create'
 
   post '/login', to: 'sessions#create'
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,7 @@ damien = User.create!(username: 'nelson13', name: 'Damien', email_address: 'exam
 
 felesia = User.create!(username: 'wife', name: 'Felesia', email_address: 'example@example', age: 27, password: 'test', password_confirmation: 'test')
 
-group = damien.groups.create!(admin_id: damien.id, name: 'testGroup', description: 'this is for testing purposes')
+damien.groups.create!(admin_id: damien.id, name: 'Nelson Family', description: 'family planning')
+damien.groups.create!(admin_id: damien.id, name: 'extended Family', description: 'whole family planner')
+
+felesia.groups.create!(admin_id: felesia.id, name: 'jenkins Family', description: 'family planning')
