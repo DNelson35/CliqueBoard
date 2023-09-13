182396af00f6506bb94580bc4ab98fecfafc6893
Damien Nelson <dhnisaman@gmail.com>
2023-09-13 14:46:38 -0400

    add middleware for group api

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,15 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../reducers/userSlice';
-import { userApi } from '../api/authSlice'
+import groupReducer from '../reducers/groupSlice';
+import { userApi } from '../api/authApi'
+import { groupApi } from '../api/groupApi';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
-    [userApi.reducerPath]: userApi.reducer
+    [userApi.reducerPath]: userApi.reducer,
+    groups: groupReducer,
+    [groupApi.reducerPath]: groupApi.reducer,
 
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware().concat(
+      userApi.middleware,
+      groupApi.middleware
+    ),
 });
 
 export default store;
