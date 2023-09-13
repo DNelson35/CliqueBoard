829b3f5532319727591c0cf56278d2a47bdf07e4
Damien Nelson <dhnisaman@gmail.com>
2023-09-13 14:43:27 -0400

    rename import, and add loading state to app comp

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -3,30 +3,35 @@ import Login from "./landing/Login"
 import Profile from './profile/Profile';
 import Welcome from './landing/Welcome';
 import Signup from './landing/Signup';
-import Groups from "./profile/Groups";
+import Group from "./profile/Group";
 import DashBoard from "./profile/DashBoard";
 import ProtectedRoutes from '../components/ProtectedRoutes';
 import {Routes, Route, useLocation} from 'react-router-dom'
-import { useCheckUserQuery } from '../api/authSlice';
+import { useCheckUserQuery } from '../api/authApi';
 import { useSelector } from 'react-redux';
+import { useGetGroupsQuery } from "../api/groupApi";
 
 function App() {
   const location = useLocation()
   const user = useSelector(state => state.user.user)
+  const groups = useSelector(state => state.groups.groups)
 
-  console.log(user)
-  useCheckUserQuery()
+  // console.log('group:', groups)
+  // console.log('user:', user?.groups)
 
-  return (
+  useGetGroupsQuery()
+  const {isLoading } = useCheckUserQuery()
+
+  return isLoading? <div>loading...</div> : (
     <div >
       {!['/login', '/signup'].includes(location.pathname) && <Nav/>}
       <Routes>
-        <Route path={'/'} element={user? <Profile/> : <Welcome/>} />
+        <Route path={'/'} element={user? <DashBoard/> : <Welcome/>} />
         <Route path={'/login'} element={<Login />} />
         <Route path={'/signup'} element={<Signup/>} />
-        <Route element={<ProtectedRoutes/>}>
+        <Route element={<ProtectedRoutes/>} >
           <Route path={'/dashboard'} element={<DashBoard/>} />
-          <Route path={'/groups'} element={<Groups/>} />
+          <Route path={'/group/:groupId'} element={<Group/>} />
           <Route path={'/profile'} element={<Profile/>} />
         </Route>
       </Routes>
