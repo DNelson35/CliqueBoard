24a01e9d19d90bae81388261945ffe56e043bc70
Damien Nelson <dhnisaman@gmail.com>
2023-09-13 14:44:08 -0400

    rename import

--- client/src/pages/landing/Login.js
+++ client/src/pages/landing/Login.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useLoginUserMutation} from "../../api/authSlice";
+import { useLoginUserMutation} from "../../api/authApi";
 
 
 const Login = () => {
--- client/src/pages/landing/Signup.js
+++ client/src/pages/landing/Signup.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSignUpUserMutation } from '../../api/authSlice'
+import { useSignUpUserMutation } from '../../api/authApi'
 
 function Signup() {
 
