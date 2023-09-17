08f6b7a0d3fe534f65478e70b1352a2e6d4d4706
Damien Nelson <dhnisaman@gmail.com>
2023-09-17 13:32:46 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :tasks
+  mount ActionCable.server => '/cable'
+
   # resources :group_widgets, only: [:create, :show, :index]
   # resources :widgets, only: [:create, :show, :index]
-  resources :users, only: [:create, :show, :update ]
+  resources :tasks
+  resources :users, only: [:update, :index ]
   resources :groups, only: [:create, :show, :index]
+  resources :invitations, only: [:create]
 
   # patch 'users/:id/upload_image', to: 'users#upload_image'
   post '/signup', to: 'users#create'
-
   post '/login', to: 'sessions#create'
-
   get '/me', to: 'users#show'
-
   delete '/logout', to: 'sessions#destroy'
 end
