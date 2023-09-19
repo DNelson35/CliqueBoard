ed017be5d176e7d2a1aec20d45489a57d61a4731
Damien Nelson <dhnisaman@gmail.com>
2023-09-19 16:21:22 -0400

    update relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :groups, foreign_key: 'admin_id', dependent: :destroy
+    has_many :group_members
     has_many :joined_groups, through: :group_members, source: :group
     has_many :sent_invitations, class_name: 'Invitation', foreign_key: 'sender_id'
     has_many :received_invitations, class_name: 'Invitation', foreign_key: 'recipient_id'
