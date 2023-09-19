53c58661bb568c02c119bb475fec0e81c116267b
Damien Nelson <dhnisaman@gmail.com>
2023-09-18 21:37:38 -0400

    comment out unused code

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -3,15 +3,15 @@ class GroupMember < ApplicationRecord
     belongs_to :user
     belongs_to :group
 
-    validates :invitation_status, presence: true, inclusion: { in: %w[pending accepted] }
-    validates :invitation_code, presence: true, uniqueness: true
-    validates :expires_at, presence: true
+    # validates :invitation_status, presence: true, inclusion: { in: %w[pending accepted] }
+    # validates :invitation_code, presence: true, uniqueness: true
+    # validates :expires_at, presence: true
 
-    before_create :generate_invitation_code
+    # before_create :generate_invitation_code
 
-    private
+    # private
 
-    def generate_invitation_code
-      self.invitation_code = SecureRandom.hex(6)
-    end
+    # def generate_invitation_code
+    #   self.invitation_code = SecureRandom.hex(6)
+    # end
 end
