8ca1bfd72e3542a7ca2918ef74a006f663dbeb76
Damien Nelson <dhnisaman@gmail.com>
2023-09-21 13:48:10 -0400

    create new migration to add status to user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_15_180124) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_164900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_180124) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "widget_data", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 
 damien = User.create!(username: 'nelson13', name: 'Damien', email_address: 'example@example.com', age: 30, password: 'test', password_confirmation: 'test')
 
-felesia = User.create!(username: 'wife', name: 'Felesia', email_address: 'example@example', age: 27, password: 'test', password_confirmation: 'test')
+felesia = User.create!(username: 'wife', name: 'Felesia', email_address: 'example@example', age: 27, password: 'test', password_confirmation: 'test', status: 'Online')
 
 family = damien.groups.create!(admin_id: damien.id, name: 'Nelson Family', description: 'family planning', access_code: 'test')
 extended = damien.groups.create!(admin_id: damien.id, name: 'extended Family', description: 'whole family planner', access_code: 'test2')
 
 family.group_members.create!(user_id: damien.id, group_id: family.id)
+family.group_members.create!(user_id: felesia.id, group_id: family.id)
 extended.group_members.create!(user_id: damien.id, group_id: extended.id)
 
 jenkins = felesia.groups.create!(admin_id: felesia.id, name: 'jenkins Family', description: 'family planning', access_code: 'test3')
