f370cc634a88932b4edbe971c5e6c65dbf785e25
Damien Nelson <dhnisaman@gmail.com>
2023-09-21 13:47:04 -0400

    Display online users

--- client/src/pages/profile/DashBoard.js
+++ client/src/pages/profile/DashBoard.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import OnlineUsersReceiver from '../../components/OnlineUsersReceiver'
 
 function DashBoard() {
   
   return (
     <div className='h-screen'>
         <div className='flex justify-evenly'>
-            <div className='border border-red-500 w-1/4 h-40'>online users</div>
+            <div className='border border-red-500 w-1/4 h-40'>
+              <h1>Online Users</h1>
+              <OnlineUsersReceiver /> 
+            </div>
             <div className='border border-red-500 w-1/4 h-40'> notifications</div>
         </div>
         <div className='flex justify-center h-full mt-10'>
-        {/* TODO: add a list of all the users events from all groups */}
             <div className='flex flex-col w-3/4 h-1/3'>
               <div className='flex-grow border border-red-500'>
                   events
