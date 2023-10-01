8a27b4c4badaadad5e1d004e3538ed7cc8a41518
Damien Nelson <dhnisaman@gmail.com>
2023-10-01 11:31:49 -0400

    change serializer group to show widget and data

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,5 +1,9 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :users
+  attributes :id, :name, :description, :users, :widgets
 
-
+  def widgets
+    object.widgets.each_with_object({}) do |widget, hash|
+      hash[widget.name] = widget.widget_data.where(group_id: object.id)
+    end
+  end
 end
