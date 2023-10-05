b3bcf0c412d141b8806d2231e72c7b0fdd8eb300
Damien Nelson <dhnisaman@gmail.com>
2023-10-05 13:41:59 -0400

    fill in event slot with all group events

--- client/src/pages/profile/DashBoard.js
+++ client/src/pages/profile/DashBoard.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import OnlineUsersReceiver from '../../components/OnlineUsersReceiver'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
 
 function DashBoard() {
+  const groups = useSelector(state => state.groups.groups)
+
+  const allEvents = groups?.map(group => group.widgets.Calendar)
+
+  const eventList = allEvents.flat().map(event => (
+    <li key={event.created_at}>
+      <p>{event.start_date}</p>
+      <p>{event.title}</p>
+    </li>
+  ))
   
   return (
     <div className='h-screen'>
@@ -16,6 +27,9 @@ function DashBoard() {
             <div className='flex flex-col w-3/4 h-1/3'>
               <div className='flex-grow border border-red-500'>
                   events
+                  <ul>
+                    {eventList}
+                  </ul>
               </div>
               <div className='flex-grow border border-red-500 mt-10'>
                   task
