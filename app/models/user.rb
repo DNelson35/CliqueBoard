9ae658208c09c4201ba0f96aaa2c41428691ab2a
Damien Nelson <dhnisaman@gmail.com>
2023-10-08 23:46:28 -0400

    create and update models for messenger

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
     has_many :joined_groups, through: :group_members, source: :group
     has_many :sent_invitations, class_name: 'Invitation', foreign_key: 'sender_id'
     has_many :received_invitations, class_name: 'Invitation', foreign_key: 'recipient_id'
+    has_many :messages
+    has_many :conversations
     #  the joined_groups can be changed to a regular has many through this is speciffcly for naming the association to joined_groups instead
 end
