ec90624907887627c92d0b9c984a20b9b665b394
Damien Nelson <dhnisaman@gmail.com>
2023-10-08 23:47:42 -0400

    update schema to include new tables for messenger

---
+++ db/migrate/20231008200100_create_conversations.rb
@@ -0,0 +1,9 @@
+class CreateConversations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :conversations do |t|
+      t.references :user1, foreign_key: {to_table: :users}
+      t.references :user2, foreign_key: {to_table: :users}
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_22_170224) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_08_200228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.bigint "user1_id"
+    t.bigint "user2_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user1_id"], name: "index_conversations_on_user1_id"
+    t.index ["user2_id"], name: "index_conversations_on_user2_id"
+  end
 
   create_table "group_members", force: :cascade do |t|
     t.bigint "user_id"
@@ -46,6 +55,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_170224) do
     t.index ["invitation_code"], name: "index_invitations_on_invitation_code", unique: true
     t.index ["recipient_id"], name: "index_invitations_on_recipient_id"
     t.index ["sender_id"], name: "index_invitations_on_sender_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -93,11 +112,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_22_170224) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "conversations", "users", column: "user1_id"
+  add_foreign_key "conversations", "users", column: "user2_id"
   add_foreign_key "group_members", "groups"
   add_foreign_key "group_members", "users"
   add_foreign_key "groups", "users", column: "admin_id"
   add_foreign_key "invitations", "users", column: "recipient_id"
   add_foreign_key "invitations", "users", column: "sender_id"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "tasks", "group_members"
   add_foreign_key "tasks", "groups"
   add_foreign_key "tasks", "users"
