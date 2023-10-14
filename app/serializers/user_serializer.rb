f0e85e87f559f23393138caf56d30a2b20d61cdd
Damien Nelson <dhnisaman@gmail.com>
2023-10-14 15:07:52 -0400

    update serializer to include conversations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,21 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :name, :email_address, :age, :password_digest, :joined_groups, :received_invitations
+  attributes :id, :username, :name, :email_address, :age, :password_digest, :received_invitations, :conversations, :joined_groups
+
+  # has_many :joined_groups
+
+  def conversations
+    if instance_options[:include_conversations] == false
+      nil
+    else
+      Conversation.where('user1_id = ? OR user2_id = ?', object.id, object.id).map do |conversation|
+        ConversationSerializer.new(conversation).as_json
+      end
+    end
+  end
+
+  # def joined_groups
+  #   object.joined_groups.includes(:users).map do |group|
+  #     group.as_json.merge(users: group.users)
+  #   end
+  # end
 end
