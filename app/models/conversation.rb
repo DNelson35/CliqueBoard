eeb088f5d0d8712a42f96ca980bf5f3bd307e32c
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:16:54 -0400

    add condition for title2

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,13 +1,18 @@
 class Conversation < ApplicationRecord
     belongs_to :user1, class_name: 'User'
-    belongs_to :user2, class_name: 'User'
+    belongs_to :user2, class_name: 'User', optional: true
+    belongs_to :group, optional: true
     has_many :messages
 
     def title1
-        user1.username
+        user1.username || group.group_name
     end
 
     def title2
-        user2.username
+        if user2.nil?
+            group.group_name
+        else
+            user2.username
+        end
     end
 end
