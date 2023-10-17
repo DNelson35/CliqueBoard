5245f5ee4cdb1da8dfaf15d7045807d94ceb4a0a
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:18:18 -0400

    update handling of conversations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
     has_many :received_invitations, class_name: 'Invitation', foreign_key: 'recipient_id'
     has_many :messages
     has_many :conversations
+    has_many :started_conversations, class_name: 'Conversation', foreign_key: 'user1_id'
+    has_many :joined_conversations, class_name: 'Conversation', foreign_key: 'user2_id'
     #  the joined_groups can be changed to a regular has many through this is speciffcly for naming the association to joined_groups instead
 
+    #TODO: evalute if the conversations through messages works as expected
+
+    def all_conversations
+        started_conversations.or(joined_conversations)
+    end
 end
