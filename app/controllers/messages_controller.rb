49dcff8349a071b9ee23febeb9235a06ebcdab20
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:16:26 -0400

    change to divide code  to conversation controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,31 +1,50 @@
 class MessagesController < ApplicationController
 
+    # def create
+    #     chat = find_or_create_conversation
+    #     message = @current_user.messages.new(message_params)
+    #     message.conversation = chat
+
+    #     if message.save!
+    #     ConversationChannel.broadcast_to(chat, message)
+    #     puts 'message was successfully'
+    #     end
+
+    # end
+    # # def create
+    # #     @message = Message.create(message_params)
+    # #     @conversation = Conversation.find(@message[:conversation_id])
+    # #     ConversationChannel.broadcast_to(@conversation, @message)
+    # #     render json: @message
+    # # end
+
     def create
-        recipient_id = params[:recipient_id].to_i
-        current_user_id = @current_user.id
-        # chat = Conversation.find_by(user1_id: [@current_user.id, params[:recipient_id]], user2_id: [params[:recipient_id], @current_user])
-        chat = Conversation.where("(user1_id = :current_user AND user2_id = :recipient) OR (user1_id = :recipient AND user2_id = :current_user)", current_user: current_user_id, recipient: recipient_id).first
+        chat = Conversation.find(params[:id])
 
-        unless chat
-            chat = Conversation.create(user1_id: @current_user.id, user2_id: params[:recipient_id])
-        end
-
-        message = @current_user.messages.new(message_params)
-        message.conversation = chat
-
-        if message.save!
-        # ConversationChannel.broadcast_to(chat, message)
-        puts 'message was successfully'
-        end
-
+        message = @current_user.messages.create(body: params[:body], conversation_id: chat.id)
+        ActionCable.server.broadcast("ConversationChannel_#{chat.id}", message)
+        
     end
     
-
     private
 
     def message_params
-        params.require(:message).permit(:body)
+        params.require(:message).permit(:body, :id)
     end
+
+    # def find_or_create_conversation
+    #     chat = if params[:chat_type] == "User"
+    #              @current_user.all_conversations.find_by(chat_type: params[:chat_type], user1_id: @current_user.id, user2_id: params[:recipient_id])
+    #            else
+    #              @current_user.all_conversations.find_by(chat_type: params[:chat_type], group_id: params[:recipient_id])
+    #            end
+      
+    #     unless chat
+    #       chat = @current_user.conversations.create(chat_type: params[:chat_type], user1_id: @current_user.id, user2_id: params[:chat_type] == "User" ? params[:recipient_id] : nil, group_id: params[:chat_type] == "Group" ? params[:recipient_id] : nil)
+    #     end
+      
+    #     chat
+    #   end
 
 
 end
