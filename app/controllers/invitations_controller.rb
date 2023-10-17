2ecf39b28cfbe6a137cd58bd0ffa6b4082084fbf
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:14:55 -0400

    update group.name to group.group_name

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,7 +2,7 @@ class InvitationsController < ApplicationController
     def create 
         recipient = User.find(params[:recipient_id])
         group = Group.find(params[:group_id])
-        message = "#{@current_user.name} wants to invite you to #{group.name} group. If you wish to join, please enter the following access key: (#{group.access_code}). "
+        message = "#{@current_user.name} wants to invite you to #{group.group_name} group. If you wish to join, please enter the following access key: (#{group.access_code}). "
         invitation = Invitation.create(sender: @current_user, recipient: recipient, message: message)
         InvitationChannel.broadcast_to(recipient, { invitation: invitation })
     end
