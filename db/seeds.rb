b49064e1e259808747ae3ce883e2f2c80f0c0d9f
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:25:57 -0400

    update to create conversations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ damien = User.create!(username: 'nelson13', name: 'Damien', email_address: 'exam
 felesia = User.create!(username: 'wife', name: 'Felesia', email_address: 'example@example', age: 27, password: 'test', password_confirmation: 'test', status: 'Online')
 
 # groups
-family = damien.groups.create!(admin_id: damien.id, name: 'Nelson Family', description: 'family planning', access_code: 'test')
-extended = damien.groups.create!(admin_id: damien.id, name: 'extended Family', description: 'whole family planner', access_code: 'test2')
-jenkins = felesia.groups.create!(admin_id: felesia.id, name: 'jenkins Family', description: 'family planning', access_code: 'test3')
+family = damien.groups.create!(admin_id: damien.id, group_name: 'Nelson Family', description: 'family planning', access_code: 'test')
+extended = damien.groups.create!(admin_id: damien.id, group_name: 'extended Family', description: 'whole family planner', access_code: 'test2')
+jenkins = felesia.groups.create!(admin_id: felesia.id, group_name: 'jenkins Family', description: 'family planning', access_code: 'test3')
 
 groups = [family, extended, jenkins]
 
@@ -26,3 +26,15 @@ groups.each do |group|
     i += 1
     group.widget_data.create!(widget_id: calendar.id, start_date: '2023-09-22', title: "event#{i}" )
 end
+
+
+# conversations
+
+me_wife = Conversation.create!(chat_type: 'User', user1_id: damien.id, user2_id: felesia.id, group_id: nil)
+me_extendeGroup = Conversation.create!(chat_type: 'Group', user1_id: damien.id, group_id: family.id)
+
+# messages
+
+to_wife = Message.create!(user_id: damien.id, conversation_id: me_wife.id, body: 'hello wife')
+
+to_group = Message.create!(user_id: damien.id, conversation_id: me_extendeGroup.id, body: 'hello group')
