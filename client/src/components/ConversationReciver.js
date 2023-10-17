01153900c79259f4cc38165f2cf2efa816085d34
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:20:33 -0400

    add receiver for conversations

---
+++ client/src/components/ConversationReciver.js
@@ -0,0 +1,30 @@
+import React, { useEffect} from 'react'
+import { useDispatch } from 'react-redux';
+import cable from '../Cable';
+import { useSelector } from 'react-redux';
+import { addMessage } from '../reducers/userSlice';
+
+function ConversationReciver() {
+
+  const conversation = useSelector((state) => state.user.conversation)
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+    const subscription = cable.subscriptions.create({ channel: 'ConversationChannel', id: conversation?.id}, {
+            received: (data) => {
+                dispatch(addMessage(data))
+            },
+        });
+
+        return () => {
+        cable.subscriptions.remove(subscription);
+        };
+    }, [conversation?.id, dispatch]);
+
+  return (
+    <></>
+  )
+}
+
+export default ConversationReciver
