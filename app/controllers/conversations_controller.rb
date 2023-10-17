94b11671983b625a9e894f4d57106753d08be56d
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:14:17 -0400

    add create to for conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,3 +1,17 @@
 class ConversationsController < ApplicationController
+    def create
+        chat = @current_user.started_conversations.create!(chat_params)
+        message = @current_user.messages.create!(body: params[:body], conversation_id: chat.id)
+        ConversationChannel.broadcast_to(chat, message)
+    end
 
+    private
+
+    def chat_params
+        params.require(:conversation).permit(:user2_id, :chat_type, :group_id)
+    end
+
+    def message_params
+        params.require(:messages).permit(:body)
+    end
 end
