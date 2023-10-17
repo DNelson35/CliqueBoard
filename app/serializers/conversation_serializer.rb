eac0d6cd1e4d75148f7a70496620969bcaf3fa6c
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:18:33 -0400

    add chat_type

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,5 +1,7 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :title1, :title2
+  attributes :id, :title1, :title2, :chat_type
   has_many :messages
+
+  # possible fix for titles is having the channel send the title down witht the conversation since the channel can gain access tot the current user.
 
 end
