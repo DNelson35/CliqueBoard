9a65159ac77ca5281ee86778e856b3b1b3dc0a21
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:26:36 -0400

    change to allow null value for user2 fk

--- db/migrate/20231008200100_create_conversations.rb
+++ db/migrate/20231008200100_create_conversations.rb
@@ -2,7 +2,7 @@ class CreateConversations < ActiveRecord::Migration[7.0]
   def change
     create_table :conversations do |t|
       t.references :user1, foreign_key: {to_table: :users}
-      t.references :user2, foreign_key: {to_table: :users}
+      t.references :user2, foreign_key: {to_table: :users}, null: true
       t.timestamps
     end
   end
