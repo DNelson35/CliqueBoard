fe355211e85220156662fdd77c45001202e56e52
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:19:36 -0400

    rework chat component

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,29 +1,63 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { useCreateMessageMutation } from '../api/authApi'
+import { useCreateConversationMutation } from '../api/authApi'
 
-function Chat({ recipient, user }) {
-  const [message, setMessage] = useState({ recipient_id: recipient.id, body: '' })
+function Chat({ recipient, chatType, user, conversation }) {
+
+  const [message, setMessage] = useState({})
   const [createMessage] = useCreateMessageMutation()
+  const [createConversation] = useCreateConversationMutation()
 
-  const conversation = user.conversations.find(chat => chat.title1 || chat.title2 === recipient.name || recipient.username )
 
-  // console.log(conversation)
-  console.log(recipient)
+  useEffect(() => {
+    if (conversation){
+      setMessage({
+        id: conversation.id,
+        body: '',
+      })
+    }else if(chatType === 'Group'){
+      setMessage({
+        group_id: recipient.id,
+        body: '',
+        chat_type: chatType,
+        user2_id: null,
+      })
+    } else {
+      setMessage({
+        user2_id: recipient.id,
+        body: '',
+        chat_type: chatType,
+        group_id: null,
+      })
+    }
+  },[conversation, chatType, recipient?.id]) 
 
   const handleSubmitChat = async (e) => {
+
+   e.preventDefault()
+   const data = await createConversation(message)
+   console.log(data)
+  }
+
+  const handleSubmitMessage = async (e) => {
     e.preventDefault()
-    await createMessage(message)
+    const response = await createMessage(message)
+    console.log(response)
   }
 
   const handleChatInput = (e) => {
-    setMessage((prevState) => ({ ...prevState, body: e.target.value }))
+    setMessage((message) => ({ ...message, body: e.target.value }))
   }
+
+  const chatMessages = conversation?.messages.map(message => <li className={`flex justify-${message.user_id === user.id ? 'end' : 'start'}`} key={message.id}>{message.body}</li>)
 
   return (
     <div>
-      <h1>{recipient.username || recipient.name}</h1>
-      <p>reciver</p>
-      <form onSubmit={handleSubmitChat}>
+      <h1>{recipient?.username || recipient?.name}</h1>
+      <ul>
+        {chatMessages}
+      </ul>
+      <form onSubmit={conversation? handleSubmitMessage : handleSubmitChat}>
         <input type='text' placeholder='Start typing...' onChange={handleChatInput}/>
         <button type='submit'></button>
       </form>
