8f545f2431e250ac863d627b43680b7ea0499413
Damien Nelson <dhnisaman@gmail.com>
2023-10-17 16:23:21 -0400

    rework messenger

--- client/src/pages/profile/Messenger.js
+++ client/src/pages/profile/Messenger.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Chat from '../../components/Chat'
 import { useMembersQuery } from '../../api/authApi'
+import { setConversation } from '../../reducers/userSlice'
+import { useDispatch } from 'react-redux'
 
 function Messenger() {
   const [searchInput, setSearchInput] = useState('')
   const [filterResults, setFilterResults] = useState({ groupList: [], userList: [] })
-  const [recipient, setRecipient] = useState(null)
-  
+  const [recipient, setRecipient] = useState({
+    data: '',
+    chat_type: '',
+  })
+  // const [conversation, setConversation] = useState(null)
+  const dispatch = useDispatch()
   const groups = useSelector((state) => state.groups.groups)
   const user = useSelector((state) => state.user.user)
   const {data: members} = useMembersQuery()
+  const conversation = useSelector((state) => state.user.conversation)
 
   const handleChange = (e) => setSearchInput(e.target.value)
 
   useEffect(() => {
     if (searchInput !== '' && members) {
-      const groupArr = groups.filter((group) => group.name.toLowerCase().includes(searchInput))
+      const groupArr = groups?.filter((group) => group.group_name.toLowerCase().includes(searchInput))
       const userArr = members?.filter((user) => user.username.toLowerCase().includes(searchInput.toLowerCase()))
 
       setFilterResults({ groupList: groupArr, userList: userArr })
@@ -25,24 +32,35 @@ function Messenger() {
     }
   }, [groups, searchInput, members])
 
-  const handleSelectedEntity = (data) => {
-    setRecipient(data)
+  const handleSelectedEntity = (data, chatType) => {
+    setRecipient({
+      data,
+      chat_type: chatType,
+    })
+
+    setConversation(null)
+  }
+
+  const handleConversationSelect = (data) => {
+    // setConversation(data)
+    dispatch(setConversation(data))
+    setRecipient(null)
   }
 
   const userFilter = filterResults.userList?.map((user) => (
-    <li key={user.id} onClick={() => handleSelectedEntity(user)}>
+    <li key={user.id} onClick={() => handleSelectedEntity(user, 'User')}>
       {user.username}
     </li>
   ))
 
   const groupsFilter = filterResults.groupList?.map((group) => (
-    <li key={group.id} onClick={() => handleSelectedEntity(group)}>
-      {group.name}
+    <li key={group.id} onClick={() => handleSelectedEntity(group, 'Group')}>
+      {group.group_name}
     </li>
   ))
 
   const conversationList = user?.conversations.map((conversation) => (
-    <li key={conversation.id} onClick={() => handleSelectedEntity(conversation)}>
+    <li key={conversation.id} onClick={() => handleConversationSelect(conversation)}>
       {conversation.title1 === user.username ? conversation.title2 : conversation.title1}
     </li>
   ))
@@ -64,9 +82,9 @@ function Messenger() {
         </div>
       </div>
       <div className='flex flex-col w-3/4 h-screen border border-red-500'>
-        {recipient? 
+        {recipient?.data || conversation ? 
         <>
-          <Chat recipient={recipient} user={user}/>
+          <Chat recipient={recipient?.data} chatType={recipient?.chat_type} conversation={conversation} user={user}/>
         </>
         : 
         <h1 className='flex justify-center'>Start a conversation</h1>}
