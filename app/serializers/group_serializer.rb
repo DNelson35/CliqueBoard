fe92a4d413a40317520b4836c05be385790dbd40
Damien Nelson <dhnisaman@gmail.com>
2023-10-21 15:49:30 -0400

    change return data of groups widgets

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,9 +1,15 @@
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :group_name, :description, :users, :widgets
 
+  # def widgets
+  #   object.widgets.each_with_object({}) do |widget, hash|
+  #     hash[widget.name] = widget.widget_data.where(group_id: object.id)
+  #   end
+  # end
+
   def widgets
     object.widgets.each_with_object({}) do |widget, hash|
-      hash[widget.name] = widget.widget_data.where(group_id: object.id)
+      hash[widget.name] = ActiveModel::SerializableResource.new(widget.widget_data.where(group_id: object.id)).as_json
     end
   end
 end
--- app/serializers/widget_datum_serializer.rb
+++ app/serializers/widget_datum_serializer.rb
@@ -1,7 +1,8 @@
 class WidgetDatumSerializer < ActiveModel::Serializer
-  attributes :id, :group_id, :widget_id, :created_at, :updated_at, :data_key, :data_value, :widget_name
+  attributes :id, :group_id, :widget_id, :created_at, :updated_at, :data_key, :data_value, :title, :event_group, :description, :start_date, :end_date
 
-  def widget_name
-    object.widget.name
+  def event_group
+    object.group.group_name
   end
+
 end
