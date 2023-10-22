1a92eb963546000aeaa6b91c0238fa3b9aa45ce6
Damien Nelson <dhnisaman@gmail.com>
2023-10-21 21:55:47 -0400

    remove event group method, added to table

--- app/serializers/widget_datum_serializer.rb
+++ app/serializers/widget_datum_serializer.rb
@@ -1,8 +1,4 @@
 class WidgetDatumSerializer < ActiveModel::Serializer
   attributes :id, :group_id, :widget_id, :created_at, :updated_at, :data_key, :data_value, :title, :event_group, :description, :start_date, :end_date
 
-  def event_group
-    object.group.group_name
-  end
-
 end
