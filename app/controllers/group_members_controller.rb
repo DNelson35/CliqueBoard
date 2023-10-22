f7691c7f82ff018d6c152e114ecea47a76af8f33
Damien Nelson <dhnisaman@gmail.com>
2023-10-21 21:54:02 -0400

    comment code save for later use

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -1,12 +1,12 @@
 class GroupMembersController < ApplicationController
-    def accept_invitation
-        invitation = Membership.find_by(invitation_code: params[:invitation_code])
+    # def accept_invitation
+    #     invitation = Membership.find_by(invitation_code: params[:invitation_code])
 
-        if invitation.nil?
-          redirect_to root_path, alert: "Invalid invitation"
-        else
-          invitation.update(invitation_status: "accepted")
-          redirect_to group_path(invitation.group_id), notice: "Invitation accepted successfully"
-        end
-    end
+    #     if invitation.nil?
+    #       redirect_to root_path, alert: "Invalid invitation"
+    #     else
+    #       invitation.update(invitation_status: "accepted")
+    #       redirect_to group_path(invitation.group_id), notice: "Invitation accepted successfully"
+    #     end
+    # end
 end
