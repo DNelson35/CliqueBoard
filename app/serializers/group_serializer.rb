f9c10bb23822039c2a33dd91de08512097271277
Damien Nelson <dhnisaman@gmail.com>
2023-10-24 21:36:54 -0400

    include conversation for group in serializer

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,11 +1,7 @@
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :group_name, :description, :users, :widgets
 
-  # def widgets
-  #   object.widgets.each_with_object({}) do |widget, hash|
-  #     hash[widget.name] = widget.widget_data.where(group_id: object.id)
-  #   end
-  # end
+  has_one :conversation
 
   def widgets
     object.widgets.each_with_object({}) do |widget, hash|
