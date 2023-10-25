99f9142545bebe2fcf5d044bf12cb7b79b331b19
Damien Nelson <dhnisaman@gmail.com>
2023-10-24 21:34:45 -0400

    add create to conversation controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,11 +1,30 @@
 class ConversationsController < ApplicationController
     def create
-        chat = @current_user.started_conversations.create!(chat_params)
-        message = @current_user.messages.create!(body: params[:body], conversation_id: chat.id)
-        ConversationChannel.broadcast_to(chat, message)
+        chat = chat_type_conditions
+
+        if chat 
+            render json: {errors: 'already exist', chat: chat}, status: :unprocessable_entity
+        else
+            create_chat
+        end
     end
 
     private
+
+    def chat_type_conditions
+        if params[:chat_type] == 'User'
+            @current_user.all_conversations.where('user1_id = ? OR user2_id = ?', params[:user2_id], params[:user2_id]).first
+        elsif params[:chat_type] == 'Group'
+            @current_user.all_conversations.where(group_id: params[:group_id]).first
+        end
+    end
+
+    def create_chat
+        chat = @current_user.started_conversations.create!(chat_params)
+        message = @current_user.messages.create!(body: params[:body], conversation_id: chat.id)
+        ConversationChannel.broadcast_to(chat, message)
+        render json: chat, status: :created
+    end
 
     def chat_params
         params.require(:conversation).permit(:user2_id, :chat_type, :group_id)
