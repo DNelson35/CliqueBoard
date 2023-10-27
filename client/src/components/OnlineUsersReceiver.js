e8a4265f80807e58c9d8e9295818d8a2ce06e091
Damien Nelson <dhnisaman@gmail.com>
2023-10-27 12:34:56 -0400

    update get groups form store

--- client/src/components/OnlineUsersReceiver.js
+++ client/src/components/OnlineUsersReceiver.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import cable from '../Cable'
 import { setOnlineUsers } from '../reducers/userSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 function OnlineUsersReceiver() {
     // const [onlineUsers, setOnlineUsers] = useState(null)
     // if i set this in redux i can use it in the groups to show if the users in a group are also online or offline by having a useEffect that triggers anytime the state changes for the online users and have it show a online or offline message if any of the users are in the online users state.
     const dispatch = useDispatch()
+    const groups = useSelector((state) => state.groups.groups)
     useEffect(() => {
         const subscription = cable.subscriptions.create('OnlineUsersChannel', {
           received: (data) => {
@@ -17,7 +18,7 @@ function OnlineUsersReceiver() {
           cable.subscriptions.remove(subscription)
         }
 
-    }, [dispatch])
+    }, [dispatch, groups])
     
   return (
     <></>
