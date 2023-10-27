4842d931fc59e65d3de77376f1a6c24f332f57e5
Damien Nelson <dhnisaman@gmail.com>
2023-10-27 12:28:23 -0400

    set creation of some data on group create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,19 @@
 require 'securerandom'
 class GroupsController < ApplicationController
-
 # TODO: add functionality to remove members from the group
 
     def create 
+        calendar = Widget.find_by(name: 'Calendar')
+        current_date = Date.current
+        formatted_date = current_date.strftime("%Y-%m-%d")
+
         code = generate_access_code
         group = @current_user.groups.create!(group_params.merge(access_code: code))
+
         group.group_members.create!(user_id: @current_user.id, group_id: group.id)
-        # group.conversation.create!(user1: @currrent_user.id, group_id: group.id, chat_type: 'Group')
+        Conversation.create!(user1_id: @current_user.id, group_id: group.id, chat_type: 'Group')
+        group.widget_data.create!(widget_id: calendar.id, start_date: "#{formatted_date}", title: "created #{group.group_name}", event_group: group.group_name )
+
         render json: group, status: :created
     end
 
@@ -31,28 +37,13 @@ class GroupsController < ApplicationController
         group = Group.find_by(access_code: params[:access_code])
         if group
             member = group.group_members.create!(user_id: @current_user.id)
+            ActionCable.server.broadcast("JoinedUserChannel_#{group.id}", @current_user)
             render json: group, status: :created
         else
             render json: {error: "invalid access code"}, status: :unauthorized
         end
     end
 
-      
-
-
-    # TODO: expample of how to implement the invitation invite with the experation. this has been moved to the invitation controller logic for the experation still needs to be implemented
-
-    # def invite 
-    #     group = Group.find(params[:id])
-    #     invited_user = User.find_by(username: params[:username])
-
-    #     if invited_user.nil? || group.users.include?(invited_user)
-    #         render json: { errors: "User not found or already a member" }, status: :unprocessable_entity
-    #     else 
-    #         invitation = group.memberships.create!(user: invited_user, invitation_status: "pending", expires_at: 48.hours.from_now)
-    #         render json: { message: "Invitation sent successfully" }, status: :ok
-    #     end
-    # end
 
     private
 
