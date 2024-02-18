4154aad6ecfbbabd5162554eee96b5e47f1e5afd
Damien Nelson <dhnisaman@gmail.com>
2024-02-17 23:01:10 -0500

    remove unused code

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -1,19 +1,14 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux/es/hooks/useSelector'
-import EditUserForm from '../../components/EditUserForm'
-import { FiEdit2 } from 'react-icons/fi'
 
 function Profile() {
   const user = useSelector(state => state.user.user)
-  const [edit, setEdit] = useState(false)
 
-
-  return edit? <EditUserForm setEdit={setEdit}/> : (
+  return (
    <div className='flex h-screen w-full justify-center overflow-hidden bg-slate-900 text-white'>
     <div className='flex-col'>
       <div className='flex justify-center space-x-1'>
         <h1 className='text-5xl uppercase text-purple-400 font-bold'>{user.name}</h1>
-        <FiEdit2 className='flex self-end text-2xl' onClick={() => setEdit(true)}/>
       </div>
       <div className='flex h-full w-screen items-center justify-evenly ml-[5%] z-10'>
         <div className='flex-col space-y-20 mb-32 z-10'>
