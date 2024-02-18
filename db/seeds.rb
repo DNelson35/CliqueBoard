00435b5d201bcfc49099ccb662db8aa6cb6945cd
Damien Nelson <dhnisaman@gmail.com>
2024-02-17 23:02:23 -0500

    update seeds to create users with status of Online

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
 # # users
-damien = User.create!(username: 'nelson13', name: 'Damien', email_address: 'example@example.com', age: 30, password: 'test', password_confirmation: 'test')
+damien = User.create!(username: 'nelson13', name: 'Damien', email_address: 'example@example.com', age: 30, password: 'test', password_confirmation: 'test', status: 'Online')
 
-felesia = User.create!(username: 'wife', name: 'Felesia', email_address: 'example@example', age: 27, password: 'test', password_confirmation: 'test')
+felesia = User.create!(username: 'wife', name: 'Felesia', email_address: 'example@example', age: 27, password: 'test', password_confirmation: 'test', status: 'Online')
 
 # # groups
 # family = damien.groups.create!(admin_id: damien.id, group_name: 'Nelson Family', description: 'family planning', access_code: 'test')
